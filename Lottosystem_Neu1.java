e6b8a4c1c42d268ab667659ecde380ca0416ce03
ManuelaWinkens <Manuela.Winkens@hhu.de>
2017-07-20 11:29:04 +0200

    Muster abprüfen und 6 Lottoreihen ausgeben

--- Lottosystem_Neu1.java
+++ Lottosystem_Neu1.java
@@ -76,39 +76,74 @@ public class Lottosystem_Neu1 {
 		return zufallsarray;
 	}
 
-	public static void main(String[] args) {
+	public static void printReihen(int[][] liste) {
+		System.out.println();
+		System.out.println("Die Zahlen fuer diese Ziehnung lauten: ");
 
-		JFrame meinFrame = new JFrame("Beispiel JFrame");
-		meinFrame.setSize(200, 200);
-		JPanel panel = new JPanel();
+		// differenz von den wertebereichen
+		int[] bereich1 = zufall(liste, 0, 7);
+		int[] bereich2 = zufall(liste, 8, 8); // 8-15
+		int[] bereich3 = zufall(liste, 16, 8); // 16-23
+		int[] bereich4 = zufall(liste, 24, 8); // 24-31
+		int[] bereich5 = zufall(liste, 32, 8); // 32-39
+		int[] bereich6 = zufall(liste, 40, 9); // 40-48
 
-		JLabel label = new JLabel("Geben Sie Ihre Lottozahlen ein");
-		panel.add(label);
-		JTextField tfName = new JTextField("Ihre Zahlen", 15);
-		panel.add(tfName);
-		
-		JButton buttonOK = new JButton("OK");
-		panel.add(buttonOK);
-		String textfeld = tfName.getText();
-		System.out.println(textfeld);
+		for (int i = 0; i < 6; i++) {
+			System.out.print(bereich1[i] + " " + bereich2[i] + " " + bereich3[i] + " " + bereich4[i] + " " + bereich5[i]
+					+ " " + bereich6[i]);
+			System.out.println();
+		}
+	}
 
-		meinFrame.add(panel);
-		meinFrame.setVisible(true);
-
-		int[][] liste = new int[49][2];
-
-		int[] lottoZahlen;
+	public static void testMuster(int[] muster, ArrayList<int[]> listOfLottoZahlen, int[] lottoZahlen) {
 		int count = 0, zahl = 0;
 		;
-		int[] xRichtige = new int[7]; // 1-6 und kein richtiger
+		int[] xRichtige = new int[7];
 		int treffer = 0;
-		String tmp = " ";
-		int[] muster = new int[6];
 
 		for (int i = 0; i < muster.length; i++) {
 			System.out.println("Bitte geben Sie eine Zahl ein und dann Enter: ");
 			muster[i] = StdIn.readInt();
 		}
+
+		// loop ueber alle lottozahlen wieviele richtige
+		for (int i = 0; i < listOfLottoZahlen.size(); i++) {
+			lottoZahlen = listOfLottoZahlen.get(i);
+			treffer = 0; // neue loop zuruecksetzen
+
+			for (int j = 0; j < 6; j++) {
+				for (int k = 0; k < 6; k++) {
+					if (muster[k] == lottoZahlen[j])
+						treffer++;
+				}
+			}
+			xRichtige[treffer]++;
+		}
+
+		System.out.println();
+		System.out.println("   3 Treffer: " + xRichtige[3]);
+		System.out.println("   4 Treffer: " + xRichtige[4]);
+		System.out.println("   5 Treffer: " + xRichtige[5]);
+		System.out.println("   6 Treffer: " + xRichtige[6]);
+	}
+
+	public static void main(String[] args) {
+
+		int[][] liste = new int[49][2];
+
+		int[] lottoZahlen;
+		int counter = 0;
+		int zahl = 0;
+
+		// int[] xRichtige = new int[7];
+		// int treffer = 0;
+		String tmp = " ";
+		int[] muster = new int[6];
+
+		// for (int i = 0; i < muster.length; i++) {
+		// System.out.println("Bitte geben Sie eine Zahl ein und dann Enter: ");
+		// muster[i] = StdIn.readInt();
+		// }
 
 		for (int i = 0; i < muster.length; i++) {
 			if (i == 5) {
@@ -116,8 +151,6 @@ public class Lottosystem_Neu1 {
 			} else {
 				tmp = tmp + String.valueOf(muster[i]) + ", ";
 			}
-			meinFrame.add(new JLabel(tmp));
-			meinFrame.setVisible(true);
 		}
 
 		ArrayList<int[]> listOfLottoZahlen = new ArrayList<int[]>();
@@ -126,7 +159,7 @@ public class Lottosystem_Neu1 {
 
 		while ((lottoZahlen = rf1.getLottoZahlen()) != null) {
 			listOfLottoZahlen.add(lottoZahlen);
-			count++;
+			counter++;
 		}
 
 		// initialisiere Array
@@ -144,23 +177,18 @@ public class Lottosystem_Neu1 {
 			}
 		}
 
-		// loop ueber alle lottozahlen wieviele richtige
-		for (int i = 0; i < listOfLottoZahlen.size(); i++) {
-			lottoZahlen = listOfLottoZahlen.get(i);
-			treffer = 0; // neue loop zuruecksetzen
-
-			for (int j = 0; j < 6; j++) {
-				for (int k = 0; k < 6; k++) {
-					if (muster[k] == lottoZahlen[j])
-						treffer++;
-				}
-			}
-			xRichtige[treffer]++;
+		System.out.println("Muster uberpruefen?: 1 eingeben.");
+		System.out.println("Ausgabe 6 Lottoreihen?: 2 eingeben.");
+		System.out.println("Infos abrufen?: 3 eingeben.");
+		int eingabe = StdIn.readInt();
+		if (eingabe == 1) {
+			testMuster(muster, listOfLottoZahlen, lottoZahlen);
+		} else if (eingabe == 2) {
+			printReihen(liste);
+		} else if (eingabe == 3) {
+			System.out.println("So arbeitet das Lottosystem: ");
+		} else {
+			System.out.println("Keine gueltige Eingabe, Programmabbruch.");
 		}
-
-		System.out.println("   3 Treffer: " + xRichtige[3]);
-		System.out.println("   4 Treffer: " + xRichtige[4]);
-		System.out.println("   5 Treffer: " + xRichtige[5]);
-		System.out.println("   6 Treffer: " + xRichtige[6]);
 	}
 }
