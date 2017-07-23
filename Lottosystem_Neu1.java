d5203a9736f87691235cd502b7c0efcd5342a21c
ManuelaWinkens <Manuela.Winkens@hhu.de>
2017-07-23 16:02:34 +0200

    Kleine Fehler ausgebessert; suchen geht nicht

--- Lottosystem_Neu1.java
+++ Lottosystem_Neu1.java
@@ -30,7 +30,7 @@ public class Lottosystem_Neu1 {
 
 			for (x = 0; x < 48; x++) { // Durchlaufen des Arrays
 
-				if (array[x][1] > array[x + 1][1]) {
+				if (array[x][1] < array[x + 1][1]) {
 
 					temp = array[x][1]; // Tausch des Wertes
 					array[x][1] = array[x + 1][1];
@@ -96,8 +96,6 @@ public class Lottosystem_Neu1 {
 	}
 
 	public static void testMuster(int[] muster, ArrayList<int[]> listOfLottoZahlen, int[] lottoZahlen) {
-		int count = 0, zahl = 0;
-		;
 		int[] xRichtige = new int[7];
 		int treffer = 0;
 
@@ -127,23 +125,34 @@ public class Lottosystem_Neu1 {
 		System.out.println("   6 Treffer: " + xRichtige[6]);
 	}
 
+	public static void search(int[][] liste, int x) {
+		boolean found = false;
+		for(int i=0; i<49; i++){
+			if(liste[i][0]==x) {
+				System.out.println("Dein gesuchter Wert " + x + " kam so oft vor: " + liste[i][1]);
+				found = true;
+			}
+		}
+		if (found==false) {
+			System.out.println("Falsche Zahl eingegeben.");
+		}
+	}
+
 	public static void main(String[] args) {
-
-		int[][] liste = new int[49][2];
-
+		// Stand 23.07.17
+		int[][] liste = { { 1, 500 }, { 2, 499 }, { 3, 513 }, { 4, 504 }, { 5, 494 }, { 6, 558 }, { 7, 508 },
+				{ 8, 457 }, { 9, 507 }, { 10, 501 }, { 11, 521 }, { 12, 482 }, { 13, 439 }, { 14, 483 }, { 15, 474 },
+				{ 16, 489 }, { 17, 512 }, { 18, 495 }, { 19, 497 }, { 20, 476 }, { 21, 472 }, { 22, 530 }, { 23, 482 },
+				{ 24, 498 }, { 25, 513 }, { 26, 547 }, { 27, 507 }, { 28, 478 }, { 29, 493 }, { 30, 485 }, { 31, 523 },
+				{ 32, 549 }, { 33, 530 }, { 34, 487 }, { 35, 490 }, { 36, 507 }, { 37, 496 }, { 38, 536 }, { 39, 511 },
+				{ 40, 495 }, { 41, 520 }, { 42, 514 }, { 43, 527 }, { 44, 486 }, { 45, 448 }, { 46, 479 }, { 47, 493 },
+				{ 48, 504 }, { 49, 543 } };
+		int[][] sortedList = sortFreq(liste);
 		int[] lottoZahlen;
 		int counter = 0;
 		int zahl = 0;
-
-		// int[] xRichtige = new int[7];
-		// int treffer = 0;
 		String tmp = " ";
 		int[] muster = new int[6];
-
-		// for (int i = 0; i < muster.length; i++) {
-		// System.out.println("Bitte geben Sie eine Zahl ein und dann Enter: ");
-		// muster[i] = StdIn.readInt();
-		// }
 
 		for (int i = 0; i < muster.length; i++) {
 			if (i == 5) {
@@ -177,17 +186,18 @@ public class Lottosystem_Neu1 {
 			}
 		}
 
-		System.out.println("Muster uberpruefen?: 1 eingeben.");
+		System.out.println("Muster ueberpruefen?: 1 eingeben.");
 		System.out.println("Ausgabe 6 Lottoreihen?: 2 eingeben.");
-		System.out.println("Infos abrufen?: 3 eingeben.");
+		System.out.println("Du suchst eine Zahl?: 3 eingeben.");
 		int eingabe = StdIn.readInt();
 		if (eingabe == 1) {
 			testMuster(muster, listOfLottoZahlen, lottoZahlen);
 		} else if (eingabe == 2) {
-			printReihen(liste);
-		} else if (eingabe == 3) {
-			System.out.println("So arbeitet das Lottosystem: ");
-		} else {
+			printReihen(sortedList);
+		} else if (eingabe==3) {
+			search(liste, 5); //funktioniert nicht
+		}
+		else {
 			System.out.println("Keine gueltige Eingabe, Programmabbruch.");
 		}
 	}
